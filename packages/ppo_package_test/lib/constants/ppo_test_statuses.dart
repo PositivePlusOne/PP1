26d8b85642613128a23921cab2b1253c9b95a703
Ryan Dixon <ryandixon1993@gmail.com>
2022-09-28 00:41:24 +0100

    Add constant test statuses

---
+++ packages/ppo_package_test/lib/constants/ppo_test_statuses.dart
@@ -0,0 +1,5 @@
+const String kTestStatusNotExecuted = 'Not Executed';
+const String kTestStatusInProgress = 'In Progress';
+const String kTestStatusPassed = 'Pass';
+const String kTestStatusFail = 'Fail';
+const String kTestStatusBlocked = 'Blocked';
