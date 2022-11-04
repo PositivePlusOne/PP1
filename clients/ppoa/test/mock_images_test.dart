cf00e0ab9ab09b2b2bf69a7d9880ff38f69c3cc8
Ryan Dixon <ryan.dixon@inqvine.com>
2022-11-04 18:40:03 +0000

    Add spider build yaml and config

---
+++ clients/ppoa/test/mock_images_test.dart
@@ -0,0 +1,8 @@
+import 'dart:io';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:ppoa/resources/resources.dart';
+
+void main() {
+  test('mock_images assets test', () {});
+}
---
+++ clients/ppoa/test/ppomock_images_test.dart
@@ -0,0 +1,8 @@
+import 'dart:io';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:ppoa/resources/resources.dart';
+
+void main() {
+  test('ppomock_images assets test', () {});
+}
