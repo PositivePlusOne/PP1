e2cecdcecb4979c52257b8c550b4f9e4ead1ef9e
Ryan Dixon <ryan.dixon@inqvine.com>
2022-11-06 10:45:15 +0000

    Add checkbox button base properties

---
+++ clients/ppoa/lib/client/components/atoms/buttons/ppo_checkbox.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:ppoa/business/state/design_system/models/design_system_brand.dart';
+import 'package:ppoa/client/components/atoms/buttons/enumerations/ppo_checkbox_style.dart';
+
+class PPOCheckbox extends StatefulWidget {
+  const PPOCheckbox({
+    required this.brand,
+    required this.onCheckboxSelected,
+    this.style = PPOCheckboxStyle.large,
+    this.isChecked = true,
+    this.isEnabled = true,
+    super.key,
+  });
+
+  final DesignSystemBrand brand;
+  final PPOCheckboxStyle style;
+
+  final Future<void> Function() onCheckboxSelected;
+  final bool isEnabled;
+  final bool isChecked;
+
+  @override
+  State<PPOCheckbox> createState() => _PPOCheckboxState();
+}
+
+class _PPOCheckboxState extends State<PPOCheckbox> {
+  Future<void> _onCheckboxTapped() async {
+    if (!mounted) {
+      return;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
