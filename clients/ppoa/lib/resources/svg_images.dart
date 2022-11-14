d46a2313623bc25c8058592eeda56d6aaf4dc062
RegalEye <66254520+RegalEye@users.noreply.github.com>
2022-11-14 11:57:50 +0000

    pp1-323 PP1 Footer Logo

--- clients/ppoa/lib/resources/svg_images.dart
+++ clients/ppoa/lib/resources/svg_images.dart
@@ -3,6 +3,7 @@ part of 'resources.dart';
 class SvgImages {
   SvgImages._();
 
+  static const String footerLogo = 'assets/images/svg/footer_logo.svg';
   static const String stampFist = 'assets/images/svg/stamp_fist.svg';
   static const String stampPlusOne = 'assets/images/svg/stamp_plus_one.svg';
   static const String stampSmile = 'assets/images/svg/stamp_smile.svg';
