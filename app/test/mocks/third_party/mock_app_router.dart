0a80fa806a9ed28b65021139ae5cb627f68936da
Ryan Dixon <ryan.dixon@inqvine.com>
2023-03-01 19:17:10 +0000

    Add tests for story 253

--- app/test/mocks/third_party/mock_app_router.dart
+++ app/test/mocks/third_party/mock_app_router.dart
@@ -7,5 +7,6 @@ import 'package:app/gen/app_router.dart';
 class MockAppRouter extends Mock implements AppRouter {
   MockAppRouter() {
     registerFallbackValue(this);
+    when(() => push(any())).thenAnswer((_) async => null);
   }
 }
