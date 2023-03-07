70ca18260ab70912e6b840096c6914ad836783da
Ryan Dixon <ryan.dixon@adalpha.io>
2023-03-07 16:31:33 +0000

    Remove iOS specific logic

--- app/lib/helpers/image_helpers.dart
+++ app/lib/helpers/image_helpers.dart
@@ -1,28 +1,27 @@
 // Dart imports:
-import 'dart:io';
 import 'dart:ui';
 
 // Package imports:
 import 'package:google_ml_kit/google_ml_kit.dart';
 
 double rotateResizeImageX(double x, InputImageRotation rotation, final Size size, final Size absoluteImageSize) {
-  final double xClamp = x.clamp(0.0, (Platform.isIOS ? absoluteImageSize.width : absoluteImageSize.height));
+  final double xClamp = x.clamp(0.0, absoluteImageSize.height);
   switch (rotation) {
     case InputImageRotation.rotation90deg:
-      return xClamp * size.width / (Platform.isIOS ? absoluteImageSize.width : absoluteImageSize.height);
+      return xClamp * size.width / absoluteImageSize.height;
     case InputImageRotation.rotation270deg:
-      return size.width - xClamp * size.width / (Platform.isIOS ? absoluteImageSize.width : absoluteImageSize.height);
+      return size.width - xClamp * size.width / absoluteImageSize.height;
     default:
       return size.width - xClamp * size.width / absoluteImageSize.width;
   }
 }
 
 double rotateResizeImageY(double y, InputImageRotation rotation, final Size size, final Size absoluteImageSize) {
-  final double yClamp = y.clamp(0.0, (Platform.isIOS ? absoluteImageSize.height : absoluteImageSize.width));
+  final double yClamp = y.clamp(0.0, absoluteImageSize.width);
   switch (rotation) {
     case InputImageRotation.rotation90deg:
     case InputImageRotation.rotation270deg:
-      return yClamp * size.height / (Platform.isIOS ? absoluteImageSize.height : absoluteImageSize.width);
+      return yClamp * size.height / (absoluteImageSize.width);
     default:
       return yClamp * size.height / absoluteImageSize.height;
   }
