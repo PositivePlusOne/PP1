bc14276e0df411366f6e52afdfd248a55fa2dc90
Ryan Dixon <ryan.dixon@inqvine.com>
2023-03-21 22:28:44 +0000

    Guard against prototype injection

--- app/functions/src/maps/profile_mappers.ts
+++ app/functions/src/maps/profile_mappers.ts
@@ -2,9 +2,30 @@ import safeJsonStringify from "safe-json-stringify";
 import { EntityRelationship } from "../services/enumerations/entity_relationship";
 
 export namespace ProfileMapper {
+  /**
+   * The properties that are displayed based on the relationship
+   * @example
+   * ```ts
+   * const enforcedRelationship = enforcedProperties[property as keyof typeof enforcedProperties];
+   * const relationshipCheck = relationship & enforcedRelationship;
+   * if (relationshipCheck === 0) {
+   *  continue;
+   * }
+   * ```
+   */
   export const enforcedProperties = {
-    id: EntityRelationship.Owner | EntityRelationship.Admin | EntityRelationship.Connected | EntityRelationship.Following | EntityRelationship.Anonymous,
-    displayName: EntityRelationship.Owner | EntityRelationship.Admin | EntityRelationship.Connected | EntityRelationship.Following | EntityRelationship.Anonymous,
+    id:
+      EntityRelationship.Owner |
+      EntityRelationship.Admin |
+      EntityRelationship.Connected |
+      EntityRelationship.Following |
+      EntityRelationship.Anonymous,
+    displayName:
+      EntityRelationship.Owner |
+      EntityRelationship.Admin |
+      EntityRelationship.Connected |
+      EntityRelationship.Following |
+      EntityRelationship.Anonymous,
     name: EntityRelationship.Owner | EntityRelationship.Admin,
     email: EntityRelationship.Owner | EntityRelationship.Admin,
     phoneNumber: EntityRelationship.Owner | EntityRelationship.Admin,
@@ -13,6 +34,12 @@ export namespace ProfileMapper {
     admin: EntityRelationship.Owner | EntityRelationship.Admin,
   };
 
+  /**
+   * Converts a profile to a response based on the relationship
+   * @param {any} profile The profile to convert
+   * @param {EntityRelationship} relationship The relationship to the profile
+   * @return {string} The profile as a response
+   */
   export function convertProfileToResponse(
     profile: any,
     relationship: EntityRelationship
@@ -21,6 +48,10 @@ export namespace ProfileMapper {
 
     //* Copy the properties that are allowed
     for (const property in profile) {
+      if (!Object.prototype.hasOwnProperty.call(profile, property)) {
+        continue;
+      }
+
       const enforcedRelationship =
         enforcedProperties[property as keyof typeof enforcedProperties];
 
