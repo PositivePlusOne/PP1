6c8f70d73e10a44527e835478a783c4775ba0bb5
dan <dan@wearearch.com>
2023-04-04 09:35:50 +0100

    wip: update branch

--- app/functions/src/constants/keys.ts
+++ app/functions/src/constants/keys.ts
@@ -3,5 +3,6 @@ export namespace Keys {
     export const StreamApiSecret = "STREAM_API_SECRET";
     export const AlgoliaApiKey = "ALGOLIA_API_KEY";
     export const AlgoliaAppID = "ALGOLIA_APP_ID";
+    export const GoogleMapKey = "MAPS_API_KEY";
     export const AlgoliaIndex = "_fl_meta_.fl_id";
 }
