93980c870fd42b4cd414d151d6c0ff6f1974dcb0
Ryan Dixon <ryan.dixon@inqvine.com>
2023-04-24 16:16:16 +0100

    Update firebase functions

--- app/lib/widgets/organisms/home/home_page.dart
+++ app/lib/widgets/organisms/home/home_page.dart
@@ -10,7 +10,6 @@ import 'package:unicons/unicons.dart';
 import 'package:app/dtos/system/design_colors_model.dart';
 import 'package:app/hooks/lifecycle_hook.dart';
 import 'package:app/providers/system/design_controller.dart';
-import 'package:app/providers/user/profile_controller.dart';
 import 'package:app/providers/user/user_controller.dart';
 import 'package:app/widgets/molecules/navigation/positive_navigation_bar.dart';
 import 'package:app/widgets/molecules/scaffolds/positive_scaffold.dart';
