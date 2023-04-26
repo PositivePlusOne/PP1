8edb1264cfdaf9132c1ed0282f9c662c2b9d2f3a
RegalEye <66254520+RegalEye@users.noreply.github.com>
2023-04-26 15:02:48 +0100

    Update listeners for profile updates

--- app/lib/widgets/organisms/account/vms/profile_edit_settings_model.dart
+++ app/lib/widgets/organisms/account/vms/profile_edit_settings_model.dart
@@ -3,6 +3,8 @@
 // Flutter imports:
 
 // Package imports:
+import 'dart:async';
+
 import 'package:app/dtos/database/user/user_profile.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:logger/logger.dart';
@@ -37,6 +39,8 @@ class AccountProfileEditSettingsViewModelState with _$AccountProfileEditSettings
 
 @riverpod
 class AccountProfileEditSettingsViewModel extends _$AccountProfileEditSettingsViewModel with LifecycleMixin {
+  StreamSubscription<UserProfile?>? userProfileSubscription;
+
   @override
   AccountProfileEditSettingsViewModelState build() {
     return AccountProfileEditSettingsViewModelState.initialState();
@@ -44,13 +48,34 @@ class AccountProfileEditSettingsViewModel extends _$AccountProfileEditSettingsVi
 
   @override
   void onFirstRender() {
-    final UserProfile profile = ref.watch(profileControllerProvider.select((value) => value.userProfile!));
+    updateVisibilityFlags();
+    updateListeners();
+    super.onFirstRender();
+  }
 
-    PositiveTogglableState birthday = PositiveTogglableState.loading;
-    PositiveTogglableState interests = PositiveTogglableState.loading;
-    PositiveTogglableState location = PositiveTogglableState.loading;
-    PositiveTogglableState genders = PositiveTogglableState.loading;
-    PositiveTogglableState hivStatus = PositiveTogglableState.loading;
+  void onUserProfileCHange(UserProfile? event) {
+    final Logger logger = ref.read(loggerProvider);
+    logger.d('[Profile Edit Settings View Model] - Attempting to update user profile listeners');
+    updateVisibilityFlags();
+  }
+
+  Future<void> updateListeners() async {
+    final Logger logger = ref.read(loggerProvider);
+    logger.d('[Profile Edit Settings View Model] - Attempting to add user profile listeners');
+
+    final ProfileController profileController = ref.read(profileControllerProvider.notifier);
+    await userProfileSubscription?.cancel();
+    userProfileSubscription = profileController.userProfileStreamController.stream.listen(onUserProfileCHange);
+  }
+
+  void updateVisibilityFlags() {
+    final UserProfile profile = ref.read(profileControllerProvider.select((value) => value.userProfile!));
+
+    PositiveTogglableState birthday = PositiveTogglableState.inactive;
+    PositiveTogglableState interests = PositiveTogglableState.inactive;
+    PositiveTogglableState location = PositiveTogglableState.inactive;
+    PositiveTogglableState genders = PositiveTogglableState.inactive;
+    PositiveTogglableState hivStatus = PositiveTogglableState.inactive;
 
     if (profile.visibilityFlags.any((element) => element == "birthday")) {
       birthday = PositiveTogglableState.active;
@@ -75,8 +100,6 @@ class AccountProfileEditSettingsViewModel extends _$AccountProfileEditSettingsVi
       toggleStateGender: genders,
       toggleStateHIVStatus: hivStatus,
     );
-
-    super.onFirstRender();
   }
 
   void onBackSelected() {
