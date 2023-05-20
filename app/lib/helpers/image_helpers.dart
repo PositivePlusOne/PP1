eedb20560bc5f371b2099f2c8820e41708d776e0
RegalEye <66254520+RegalEye@users.noreply.github.com>
2023-05-20 01:29:40 +0100

    Cleaned up android version of code

--- app/lib/helpers/image_helpers.dart
+++ app/lib/helpers/image_helpers.dart
@@ -22,40 +22,43 @@ Offset rotateResizeImage(
   // if (Platform.isIOS) {
   //   offset = Offset(offset.dy, offset.dx);
   // }
-  double ratio = (size.width / croppedImageSize.width);
-  double ratio2 = (size.height / croppedImageSize.height);
-  // return Offset(
-  //   size.width - offset.dx * 0.57 + 115,
-  //   // size.width - offset.dx * (size.width / croppedImageSize.width),
-  //   // size.width - offset.dx + (absoluteImageSize.width - size.width) / 2,
-  //   // offset.dx * size.height / absoluteImageSize.height - (504 - size.width) / 2,
 
-  //   // offset.dy * ratio2 - (size.height - 896) / 2,
-  //   offset.dy * size.width / croppedImageSize.width + 93, //+ 0.7 * (absoluteImageSize.height - size.height) / 2,
-  //   // offset.dy * 0.9 - 105, //- (absoluteImageSize.height - size.height) / 2,
-  // );
+  //? size is the size of the phone screen/rendered area
+  //? absoluteImageSize is the dimentions of the image that was sent to the face tracking software
+  //? croppedImageSize is the dimentions of the subset of the absoluteImageSize, this is the area that the face tracker has been told to look for faces in
+  //? offset is the coordinate to be mapped from the face tracking coordinate system to the display coordinate system
+
+  //? Scale factor to map coordinates from the cropped image set to the phone resolution set
+  final double ratio = (size.width / croppedImageSize.width);
+
+  //? offset marking the distance between the absolute image size and the cropped image size
+  //? offset must also be multiplied by the ratio above to be mapped from the face tracker coordintate system to the phone screen coordinate system
+  final double widthOffsetFT = (absoluteImageSize.height - croppedImageSize.width) / 2;
+  final double heightOffsetFT = (absoluteImageSize.width - croppedImageSize.height) / 2;
+
+  final double widthOffset = (size.width - (croppedImageSize.width * ratio)) / 2;
+  final double heightOffset = (size.height - (croppedImageSize.height * ratio)) / 2;
 
   switch (rotation) {
     case InputImageRotation.rotation90deg:
       return Offset(
-        offset.dx - (absoluteImageSize.width - size.width) / 2,
-        size.height - offset.dy + (absoluteImageSize.height - size.height) / 2,
+        (offset.dx - widthOffsetFT) * ratio - widthOffset,
+        size.height - ((offset.dy - heightOffsetFT) * ratio + heightOffset),
       );
     case InputImageRotation.rotation180deg:
       return Offset(
-        offset.dy - (absoluteImageSize.height - size.width) / 2,
-        offset.dx - (absoluteImageSize.width - size.height) / 2,
+        (offset.dy - widthOffsetFT) * ratio - widthOffset,
+        (offset.dx - heightOffsetFT) * ratio + heightOffset,
       );
     case InputImageRotation.rotation270deg:
       return Offset(
-        size.width - (offset.dx - (absoluteImageSize.height - croppedImageSize.width) / 2) * ratio - (size.width - (croppedImageSize.width * ratio)) / 2, // (absoluteImageSize.width - size.width) / 2,
-        (offset.dy - (absoluteImageSize.width - croppedImageSize.height) / 2) * ratio + (size.height - (croppedImageSize.height * ratio)) / 2,
-        // offset.dy * 0.592 + 105, // - (absoluteImageSize.height - size.height) / 2,
+        size.width - (offset.dx - widthOffsetFT) * ratio - widthOffset,
+        (offset.dy - heightOffsetFT) * ratio + heightOffset,
       );
     default:
       return Offset(
-        size.width - (offset.dy - (absoluteImageSize.height - size.width) / 2),
-        size.height - (offset.dx - (absoluteImageSize.width - size.height) / 2),
+        size.width - (offset.dy - widthOffsetFT) * ratio - widthOffset,
+        size.height - (offset.dx - heightOffsetFT) * ratio - heightOffset,
       );
   }
 }
