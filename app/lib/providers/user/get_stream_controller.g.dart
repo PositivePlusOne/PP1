d0661b473439d66c720ab36826f488cc7c73f2c5
andyrecitearch <andrew@wearearch.com>
2023-05-24 10:09:09 +0100

    feat: initial setup of viewing member within a stream channel

--- app/lib/providers/user/get_stream_controller.g.dart
+++ app/lib/providers/user/get_stream_controller.g.dart
@@ -7,7 +7,7 @@ part of 'get_stream_controller.dart';
 // **************************************************************************
 
 String _$getStreamControllerHash() =>
-    r'1bb2676d3c2f1855754462583330bcc5547e52d7';
+    r'958452d641cf8f775ebe1b348953219d562b80b0';
 
 /// See also [GetStreamController].
 @ProviderFor(GetStreamController)
