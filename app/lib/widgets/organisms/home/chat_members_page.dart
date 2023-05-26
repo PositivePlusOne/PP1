288c67ba1f7b2173f84e9be409d48b84c474dbdd
andyrecitearch <andrew@wearearch.com>
2023-05-26 15:25:20 +0100

    feat: add users to group messages

--- app/lib/widgets/organisms/home/chat_members_page.dart
+++ app/lib/widgets/organisms/home/chat_members_page.dart
@@ -33,6 +33,8 @@ class ChatMembersPage extends ConsumerWidget {
     final ChatViewModelState chatViewModelState = ref.watch(chatViewModelProvider);
     final DesignColorsModel colors = ref.watch(designControllerProvider.select((value) => value.colors));
 
+    print(chatViewModelState.currentChannel?.ownCapabilities);
+
     return PositiveScaffold(
       headingWidgets: [
         SliverPadding(
