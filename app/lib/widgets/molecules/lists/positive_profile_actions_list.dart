b7a729b8075641e14456a98baa1fb830869d8caa
Ryan Dixon <ryan.dixon@inqvine.com>
2023-07-02 22:37:16 +0100

    Lint runner

--- app/lib/widgets/molecules/lists/positive_profile_actions_list.dart
+++ app/lib/widgets/molecules/lists/positive_profile_actions_list.dart
@@ -2,7 +2,6 @@
 import 'dart:async';
 
 // Flutter imports:
-import 'package:app/providers/content/conversation_controller.dart';
 import 'package:flutter/material.dart';
 
 // Package imports:
@@ -19,6 +18,7 @@ import 'package:app/extensions/dart_extensions.dart';
 import 'package:app/extensions/relationship_extensions.dart';
 import 'package:app/extensions/widget_extensions.dart';
 import 'package:app/gen/app_router.dart';
+import 'package:app/providers/content/conversation_controller.dart';
 import 'package:app/providers/user/relationship_controller.dart';
 import 'package:app/services/third_party.dart';
 import 'package:app/widgets/atoms/buttons/enumerations/positive_button_style.dart';
