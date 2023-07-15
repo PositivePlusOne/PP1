a15ae12639ec8faab3a37bc8e2eaae93a9317868
Ryan Dixon <ryan.dixon@adalpha.io>
2023-07-15 15:39:43 +0100

    WIP: Tag lint

--- app/functions/src/dto/tags.ts
+++ app/functions/src/dto/tags.ts
@@ -14,12 +14,27 @@ export type TagLocalizationJSON = {
   value?: string;
 };
 
+/**
+ * Represents a tag.
+ * @class
+ * @property {string} key the tag key.
+ * @property {string} fallback the fallback value.
+ * @property {boolean} promoted true if the tag is promoted.
+ * @property {TagLocalization[]} localizations the localizations.
+ * @returns {Tag} the tag.
+ */
 export class Tag {
   key: string;
   fallback: string;
   promoted: boolean;
   localizations: TagLocalization[];
 
+  /**
+   * Creates a new tag.
+   * @param {TagJSON} json the json.
+   * @returns {Tag} the tag.
+   * @constructor
+   */
   constructor(json: TagJSON) {
     this.key = json.key || "";
     this.fallback = json.fallback || "";
@@ -27,26 +42,51 @@ export class Tag {
     this.localizations = json.localizations?.map((e) => new TagLocalization(e)) || [];
   }
 
+  /**
+   * Gets a list of tags from a json array.
+   * @param {TagJSON[]} data the json array.
+   * @returns {Tag[]} the tags.
+   */
   static fromJsonArray(data: any[]): Tag[] {
     return data.map((e) => new Tag(e));
   }
 
+  /**
+   * Gets a tag from json.
+   * @param {TagJSON} json the json.
+   * @returns {Tag} the tag.
+   */
   static fromJSON(json: TagJSON): Tag {
     return new Tag(json);
   }
 }
 
+/**
+ * Represents a tag localization.
+ * @class
+ * @property {string} locale the locale.
+ * @property {string} value the value.
+ */
 export class TagLocalization {
   locale: string;
   value: string;
 
+  /**
+   * Creates a new tag localization.
+   * @param {TagLocalizationJSON} json the json.
+   * @returns {TagLocalization} the tag localization.
+   */
   constructor(json: TagLocalizationJSON) {
     this.locale = json.locale || "";
     this.value = json.value || "";
   }
 }
 
-
+/**
+ * Formats a tag.
+ * @param {string} tag the tag.
+ * @returns {string} the formatted tag.
+ */
 export function formatTag(tag: string): string {
   return snakeCase(lowerCase(tag));
 }
