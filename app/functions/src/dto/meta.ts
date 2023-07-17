7221de52984892674863f2e9152d6d7ab05bd848
Ryan Dixon <ryan.dixon@adalpha.io>
2023-07-17 20:48:38 +0100

    Align meta DTO with database

--- app/functions/src/dto/meta.ts
+++ app/functions/src/dto/meta.ts
@@ -12,28 +12,28 @@ export interface FlMetaJSON {
 }
 
 export class FlMeta {
-    createdBy?: string;
-    createdDate?: string;
-    docId?: string;
+    created_by?: string;
+    created_date?: string;
+    doc_id?: string;
     fl_id?: string;
     env = '';
     locale = 'en';
     schema = '';
-    schemaRefId?: string;
-    updatedBy?: string;
-    updatedDate?: string;
+    schema_ref_id?: string;
+    updated_by?: string;
+    updated_date?: string;
 
     constructor(json: FlMetaJSON) {
-        this.createdBy = json.created_by;
-        this.createdDate = json.created_date;
-        this.docId = json.doc_id;
+        this.created_by = json.created_by;
+        this.created_date = json.created_date;
+        this.doc_id = json.doc_id;
         this.fl_id = json.fl_id;
         this.env = json.env || '';
         this.locale = json.locale || 'en';
         this.schema = json.schema || '';
-        this.schemaRefId = json.schema_ref_id;
-        this.updatedBy = json.updated_by;
-        this.updatedDate = json.updated_date;
+        this.schema_ref_id = json.schema_ref_id;
+        this.updated_by = json.updated_by;
+        this.updated_date = json.updated_date;
     }
 
     static fromJSON(json: FlMetaJSON): FlMeta {
