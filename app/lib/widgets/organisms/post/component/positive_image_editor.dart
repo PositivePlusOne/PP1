596d6b320c9c9570eebd7b4463846809bdeb237f
Ryan Dixon <ryan.dixon@inqvine.com>
2023-08-17 23:41:29 +0100

    WIP

--- app/lib/widgets/organisms/post/component/positive_image_editor.dart
+++ app/lib/widgets/organisms/post/component/positive_image_editor.dart
@@ -1,14 +1,69 @@
+import 'dart:typed_data';
+
+import 'package:app/constants/design_constants.dart';
+import 'package:app/extensions/number_extensions.dart';
+import 'package:app/extensions/widget_extensions.dart';
+import 'package:app/main.dart';
 import 'package:app/providers/activities/dtos/gallery_entry.dart';
+import 'package:app/services/third_party.dart';
+import 'package:app/widgets/atoms/buttons/positive_back_button.dart';
+import 'package:app/widgets/atoms/buttons/positive_button.dart';
+import 'package:app/widgets/organisms/post/create_post_page.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:logger/logger.dart';
 
-class PositiveImageEditor extends ConsumerWidget {
+class PositiveImageEditor extends StatefulHookConsumerWidget {
   const PositiveImageEditor({super.key, this.galleryEntry});
 
   final GalleryEntry? galleryEntry;
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return Container(color: Colors.brown);
+  ConsumerState<PositiveImageEditor> createState() => _PositiveImageEditorState();
+}
+
+enum PositiveEditorState {
+  loading,
+  editing,
+}
+
+class _PositiveImageEditorState extends ConsumerState<PositiveImageEditor> {
+  PositiveEditorState state = PositiveEditorState.loading;
+  Uint8List originalImageBytes = Uint8List(0);
+  Uint8List editedImageBytes = Uint8List(0);
+
+  @override
+  void initState() {
+    super.initState();
+    loadInitialGalleryEntryData();
+  }
+
+  void loadInitialGalleryEntryData() async {
+    final Logger logger = providerContainer.read(loggerProvider);
+    if (widget.galleryEntry?.data?.isEmpty ?? true) {
+      logger.e('Gallery entry data is empty');
+      return;
+    }
+
+    originalImageBytes = widget.galleryEntry!.data!;
+    editedImageBytes = widget.galleryEntry!.data!;
+    state = PositiveEditorState.editing;
+    setStateIfMounted();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final MediaQueryData mediaQueryData = MediaQuery.of(context);
+
+    return ListView(
+      children: <Widget>[
+        const PositiveBackButton(),
+        //* Add padding so that the create post page navigation bar doesn't overlap the image editor
+        kPaddingMedium.asVerticalBox,
+        kCreatePostNavigationHeight.asVerticalBox,
+        kPaddingMedium.asVerticalBox,
+        mediaQueryData.padding.bottom.asVerticalBox,
+      ],
+    );
   }
 }
