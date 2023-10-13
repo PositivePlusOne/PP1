3ff7d69ae25546c6fd2ef4b95435524a149d4ccc
RegalEye <66254520+RegalEye@users.noreply.github.com>
2023-10-13 16:14:59 +0100

    first pass at embedding video into posts

---
+++ app/lib/hooks/first_render_hook.dart
@@ -0,0 +1,45 @@
+// Dart imports:
+import 'dart:async';
+
+// Flutter imports:
+import 'package:flutter/material.dart';
+
+// Package imports:
+import 'package:event_bus/event_bus.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+// Project imports:
+import 'package:app/main.dart';
+import '../services/third_party.dart';
+
+void useFirstRenderHook({
+  required VoidCallback callback,
+}) {
+  return use(FirstRenderHook(
+    callback: callback,
+  ));
+}
+
+class FirstRenderHook extends Hook<void> {
+  const FirstRenderHook({
+    required this.callback,
+  });
+
+  final VoidCallback callback;
+
+  @override
+  HookState<void, Hook<void>> createState() {
+    return FirstRenderHookState();
+  }
+}
+
+class FirstRenderHookState extends HookState<void, FirstRenderHook> {
+  @override
+  void initHook() {
+    super.initHook();
+    WidgetsBinding.instance.addPostFrameCallback((_) => hook.callback());
+  }
+
+  @override
+  void build(BuildContext context) {}
+}
