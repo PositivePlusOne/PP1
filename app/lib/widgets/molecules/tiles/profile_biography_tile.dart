77a158ec6dfb009121578186c4d657be102387ce
Douglas Brain <butterbrain77@gmail.com>
2023-10-18 01:26:06 +0100

    PP1-537 organisation profile biography displaying weblinks now

--- app/lib/widgets/molecules/tiles/profile_biography_tile.dart
+++ app/lib/widgets/molecules/tiles/profile_biography_tile.dart
@@ -1,13 +1,18 @@
 // Flutter imports:
+import 'package:app/extensions/profile_extensions.dart';
+import 'package:app/services/third_party.dart';
 import 'package:flutter/material.dart';
 
 // Package imports:
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:flutter_linkify/flutter_linkify.dart';
 
 // Project imports:
 import 'package:app/dtos/database/profile/profile.dart';
 import 'package:app/dtos/system/design_colors_model.dart';
+import 'package:stream_chat_flutter/stream_chat_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 import '../../../constants/design_constants.dart';
 import '../../../dtos/system/design_typography_model.dart';
 import '../../../providers/system/design_controller.dart';
@@ -39,10 +44,19 @@ class ProfileBiographyTile extends ConsumerWidget {
           style: typography.styleSubtitleBold.copyWith(color: colors.colorGray3),
         ),
         const SizedBox(height: kPaddingSmallMedium),
-        Text(
-          profile.biography,
-          style: typography.styleSubtitle.copyWith(color: colors.black),
-        ),
+        // we allow links for companies so show those nice and blue
+        if (profile.isOrganisation)
+          Linkify(
+            onOpen: (link) => launchUrl(Uri.parse(link.url)),
+            text: profile.biography,
+            style: typography.styleSubtitle.copyWith(color: colors.black),
+          ),
+        // if not an org - we only allow text to be shown here
+        if (!profile.isOrganisation)
+          Text(
+            profile.biography,
+            style: typography.styleSubtitle.copyWith(color: colors.black),
+          ),
       ],
     );
   }
