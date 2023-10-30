c1b54d763de39d9f97368b5d9edc44b4808ebd06
Ryan Dixon <ryan.dixon@inqvine.com>
2023-10-30 04:06:28 +0000

    Add all flags to the users profile when promoting to organisation

--- app/functions/src/services/mixpanel_service.ts
+++ app/functions/src/services/mixpanel_service.ts
@@ -4,16 +4,16 @@ export namespace MixpanelService {
     export const mixpanelApiDocPath = "@mixpaneldevdocs/v3.26#9ex4re2zlo8xss8z";
     export const mixpanelApi = api(mixpanelApiDocPath);
 
-    export async function fetchMixpanelData(event: string, fromDate: string, toDate: string, filters?: { [key: string]: string }) {
-        const mixpanelApiSecret = process.env.MIXPANEL_API_SECRET;
-        const mixpanelProjectId = process.env.MIXPANEL_PROJECT_ID;
-        if (!mixpanelApiSecret || !mixpanelProjectId) {
-            throw new Error(`No Mixpanel API secret specified`);
-        }
+    // export async function fetchMixpanelData(event: string, fromDate: string, toDate: string, filters?: { [key: string]: string }) {
+    //     const mixpanelApiSecret = process.env.MIXPANEL_API_SECRET;
+    //     const mixpanelProjectId = process.env.MIXPANEL_PROJECT_ID;
+    //     if (!mixpanelApiSecret || !mixpanelProjectId) {
+    //         throw new Error(`No Mixpanel API secret specified`);
+    //     }
 
-        // mixpanelApi.auth(`Basic ${Buffer.from(mixpanelApiSecret).toString('base64')}`);
+    //     // mixpanelApi.auth(`Basic ${Buffer.from(mixpanelApiSecret).toString('base64')}`);
 
-        // TODO: Add these analytics closer to launch, we can most in the DB for now to emulate.
-        return {};
-    }
+    //     // TODO: Add these analytics closer to launch, we can most in the DB for now to emulate.
+    //     return {};
+    // }
 }
