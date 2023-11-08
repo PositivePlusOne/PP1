b04fa4ffc5f8179079d4adfd6fb1bbb168aa5834
Ryan Dixon <ryan.dixon@inqvine.com>
2023-11-08 14:17:43 +0000

    Add algolia local API keys for envs

--- app/lib/services/third_party.dart
+++ app/lib/services/third_party.dart
@@ -208,8 +208,15 @@ FutureOr<Algolia> algolia(AlgoliaRef ref) async {
   final Logger logger = ref.read(loggerProvider);
   logger.i('Initializing Algolia');
 
-  // TODO(ryan): Make these environmental
-  return const Algolia.init(applicationId: 'N7Q08JSQY0', apiKey: '0011036dc6c06fc2211c001146162eda');
+  final SystemController systemController = ref.read(systemControllerProvider.notifier);
+  switch (systemController.environment) {
+    case SystemEnvironment.develop:
+      return const Algolia.init(applicationId: 'N7Q08JSQY0', apiKey: '0011036dc6c06fc2211c001146162eda');
+    case SystemEnvironment.staging:
+      return const Algolia.init(applicationId: 'AWKMEQDRX7', apiKey: '5a93c4dd3739ea7086014c3d323cc59a');
+    case SystemEnvironment.production:
+      return const Algolia.init(applicationId: 'DB7J3BMYAI', apiKey: '01c205da1edb779162d0991de0f01500');
+  }
 }
 
 @Riverpod(keepAlive: true)
