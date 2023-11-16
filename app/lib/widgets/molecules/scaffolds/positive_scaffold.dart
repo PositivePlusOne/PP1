a2908f4f6ce721ac92b9e541b7cad7abc8987c15
Ryan Dixon <ryan.dixon@inqvine.com>
2023-11-16 23:56:40 +0000

    nothing to see here

--- app/lib/widgets/molecules/scaffolds/positive_scaffold.dart
+++ app/lib/widgets/molecules/scaffolds/positive_scaffold.dart
@@ -105,7 +105,8 @@ class PositiveScaffold extends ConsumerWidget {
 
   static MediaQueryData buildMediaQuery(MediaQueryData mediaQueryData) {
     return mediaQueryData.copyWith(
-      boldText: false, textScaler: const TextScaler.linear(1.0),
+      boldText: false,
+      textScaleFactor: 1.0,
     );
   }
 
